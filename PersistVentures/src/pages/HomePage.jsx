52cf1bc286c56c926eacf0684a60ce1f8d4c4ffc
Suraj <104007414+surajb0710@users.noreply.github.com>
2025-04-02 18:16:38 +0530

    Images added on homepage

--- PersistVentures/src/pages/HomePage.jsx
+++ PersistVentures/src/pages/HomePage.jsx
@@ -7,16 +7,28 @@ import SetApart from '../components/SetApart';
 import TrustedPartner from '../components/TrustedPartner';
 import WeDoEverything from '../components/WeDoEverything';
 import React, { useRef } from 'react';
+import linkedin from '../assets/images/linkedin_01.png';
+import typescript from '../assets/images/typescript.png';
 
 const HomePage = () => {
   const section1Ref = useRef(null);
 
   const scrollToSection = (ref) => {
-    ref.current?.scrollIntoView({ behavior: 'smooth' }); // Smooth scrolling
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <main className="flex flex-col items-center">
+      <div className="absolute -right-20 top-50 blur-xs">
+        <img src={linkedin} alt="" className="w-60 aspect-square rotate-15" />
+      </div>
+      <div className="absolute -left-20 top-120 blur-xs">
+        <img
+          src={typescript}
+          alt=""
+          className="w-60 aspect-square -rotate-15"
+        />
+      </div>
       <HeroComponent section1Ref={section1Ref} />
       <TrustedPartner />
       <SetApart targetRef={section1Ref} onNavigate={scrollToSection} />
